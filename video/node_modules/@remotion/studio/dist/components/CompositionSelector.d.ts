a3a3356134b0d5977c3a1ee1e98a788bdba98dac
Nick <nick@Nicks-MacBook.local>
2026-05-25 15:53:42 -0700

    feat: initialize video workspace with Remotion + msedge-tts

---
+++ video/node_modules/@remotion/studio/dist/components/CompositionSelector.d.ts
@@ -0,0 +1,3 @@
+import React from 'react';
+export declare const getKeysToExpand: (initialFolderName: string, parentFolderName: string | null, initial?: string[]) => string[];
+export declare const CompositionSelector: React.FC;
