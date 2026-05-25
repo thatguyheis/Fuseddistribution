dec7b707d31dd729a1c48d9b653126a77153ece7
Nick <nick@Nicks-MacBook.local>
2026-05-25 15:54:54 -0700

    chore: add video .gitignore, untrack node_modules and generated dirs

--- video/node_modules/@remotion/studio/dist/components/CompositionSelector.d.ts
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-export declare const getKeysToExpand: (initialFolderName: string, parentFolderName: string | null, initial?: string[]) => string[];
-export declare const CompositionSelector: React.FC;
--- video/node_modules/msedge-tts/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { MsEdgeTTS, Voice, ProsodyOptions } from "./MsEdgeTTS";
-export { OUTPUT_FORMAT } from "./OUTPUT_FORMAT";
-export { PITCH } from "./PITCH";
-export { RATE } from "./RATE";
-export { VOLUME } from "./VOLUME";
