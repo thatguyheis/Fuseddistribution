52cd3850982cf766c636528d5e1a89d8ffaa23b9
Nick <nick@Nicks-MacBook.local>
2026-05-29 13:51:12 -0700

    fix(video): upgrade Remotion to 4.0.469, fix Poppins loadFont style param

--- video/src/fonts.ts
+++ video/src/fonts.ts
@@ -2,4 +2,4 @@ import { loadFont as loadBebasNeue } from "@remotion/google-fonts/BebasNeue";
 import { loadFont as loadPoppins } from "@remotion/google-fonts/Poppins";
 
 export const { fontFamily: BEBAS } = loadBebasNeue();
-export const { fontFamily: POPPINS } = loadPoppins({ weights: ["400", "600"] });
+export const { fontFamily: POPPINS } = loadPoppins("normal", { weights: ["400", "600"] });
